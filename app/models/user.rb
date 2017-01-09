32e9cf863bc1425d0c7f6a1ff9af0587279ad263
Raymond Lee <raylee0525@gmail.com>
2017-01-08 23:54:01 -0800

    Implement back-end authentication

---
+++ app/models/user.rb
@@ -0,0 +1,48 @@
+class User < ActiveRecord::Base
+  validates :username, :password_digest, :session_token, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: { minimum: 8, allow_nil: true }
+
+  attr_reader :password
+
+  after_initialize :ensure_session_token
+  before_validation :ensure_session_token_uniqueness
+
+
+  def password=(password)
+    self.password_digest = BCrypt::Password.create(password)
+    @password = password
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token!
+    self.session_token = new_session_token
+    ensure_session_token_uniqueness
+    self.save!
+    self.session_token
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user && user.is_password?(password) ? user : nil
+  end
+
+  private
+
+  def new_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def ensure_session_token
+    self.session_token ||= new_session_token
+  end
+
+  def ensure_session_token_uniqueness
+    while User.find_by(session_token: self.session_token)
+      self.session_token = new_session_token
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  root "static_pages#root"  
+  root "static_pages#root"
+
+  namespace :api, defaults: { format: :json } do
+    resources :users, only: [:create]
+    resource :session, only: [:create, :destroy, :show]
+  end
 end
