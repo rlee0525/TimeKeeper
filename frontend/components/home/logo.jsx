015be33ebe081f04cd2fc022020ff1563b1dd6e0
Raymond Lee <raylee0525@gmail.com>
2017-01-10 18:35:44 -0800

    Fixed home timer

--- frontend/components/home/logo.jsx
+++ frontend/components/home/logo.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default () => (
   <div className="logo">
-    <img src="http://res.cloudinary.com/rlee0525/image/upload/v1484000164/1_Primary_logo_on_transparent_267x65_pv2xex.png" />
+    <Link to={"/"}>
+      <img src="http://res.cloudinary.com/rlee0525/image/upload/v1484000164/1_Primary_logo_on_transparent_267x65_pv2xex.png" />
+    </Link>
   </div>
 );
--- frontend/components/timer/home_timer.jsx
+++ frontend/components/timer/home_timer.jsx
@@ -5,23 +5,45 @@ class HomeTimer extends React.Component {
     super(props);
 
     this.state = {
-      elapsed: 0
+      elapsed: 0,
+      timerStatus: true,
+      stoppedTime: null
     };
 
     this.tick = this.tick.bind(this);
+    this.handleTimerStatus = this.handleTimerStatus.bind(this);
   }
 
   componentDidMount() {
-    setInterval(this.tick, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.tick);
+    setInterval(this.tick, 100);
   }
 
   tick() {
     let start = this.props.start;
-    this.setState({ elapsed: new Date() - start });
+
+    if (this.state.timerStatus === true) {
+      if (this.state.stoppedTime) {
+        let diff = this.state.stoppedTime;
+        this.setState({ elapsed: Date.now() - this.state.stoppedTime });
+      } else {
+        this.setState({ elapsed: Date.now() - start });
+      }
+    } else {
+      let elapsed = this.state.elapsed;
+      this.setState({ elapsed });
+    }
+  }
+
+  handleTimerStatus() {
+    let timerStatus = !this.state.timerStatus;
+    if (timerStatus === true) {
+      this.setState({
+        timerStatus,
+        stoppedTime: Date.now() - this.state.elapsed
+      });
+    } else {
+      this.setState({ timerStatus });
+    }
   }
 
   displayTime(time) {
@@ -35,16 +57,38 @@ class HomeTimer extends React.Component {
     } else {
       minutes = Math.floor(seconds / 60);
       seconds = seconds - (minutes * 60);
+      if (minutes < 2) {
+        return `${minutes} minute ${seconds} seconds`;
+      } else {
+        return `${minutes} minutes ${seconds} seconds`;
+      }
+    }
+  }
 
-      return `${minutes} minutes ${seconds} seconds`;
+  displayCurrentLocation() {
+    if (window.location.hash === "#/") {
+      return " TimeKeeper! ";
+    } else if (window.location.hash === "#/signup") {
+      return " Signup Page! ";
+    } else if (window.location.hash === "#/login") {
+      return " Login Page! ";
     }
   }
 
   render() {
+    let buttonName = this.state.timerStatus === true ? "Stop" : "Start";
+
     return (
-      <div className="home-timer">
-        Reading TimeKeeper.. {this.displayTime(this.state.elapsed)}
-        <button>Stop</button>
+      <div className="home-timer-box">
+        <span className="home-timer-text">
+          Reading {this.displayCurrentLocation()}
+        </span>
+        <span className="home-timer">
+          {this.displayTime(this.state.elapsed)}
+        </span>
+        <button className="timer-button" onClick={this.handleTimerStatus}>
+          { buttonName }
+        </button>
       </div>
     );
   }
