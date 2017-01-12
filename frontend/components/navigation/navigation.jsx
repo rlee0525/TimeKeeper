226702275bfbb5e70398d6b36a610d2ca22b483b
Raymond Lee <raylee0525@gmail.com>
2017-01-12 15:56:57 -0800

    Updated Navbar css and icons

--- frontend/components/navigation/navigation.jsx
+++ frontend/components/navigation/navigation.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, withRouter, hashHistory } from 'react-router';
+import FontAwesome from 'react-fontawesome';
 
 class Navigation extends React.Component {
   constructor(props) {
@@ -15,24 +16,33 @@ class Navigation extends React.Component {
   render() {
     return (
       <div className="navigation-bar">
-        <div className="nav-logo">
-          <img src="http://res.cloudinary.com/rlee0525/image/upload/c_scale,w_400/v1484258095/Logomakr_6yUYIl_hmu0oi.png" />
+        <div className="nav-menu">
+          <div className="nav-logo">
+            <Link to="/"><img src="http://res.cloudinary.com/rlee0525/image/upload/c_scale,w_400/v1484258095/Logomakr_6yUYIl_hmu0oi.png" /></Link>
+          </div>
+
+          <div className="nav-tabs">
+            <button><Link to="/timer">Timer</Link></button>
+            <button>Projects</button>
+            <button>Coworkers</button>
+            <button>Tags</button>
+            <button>Help</button>
+          </div>
         </div>
 
-        <h1>Timer</h1>
-        <h1>Projects</h1>
-        <h1>Co-workers</h1>
-        <h1>Tags</h1>
-        <h1>Help</h1>
-
-        <div className="profile-picture">
-          <img src="http://res.cloudinary.com/rlee0525/image/upload/c_scale,w_75/v1484258090/userIcon_i3rk1y.png" />
+        <div className="current-user-logout">
+          <div className="profile-picture">
+            <FontAwesome className='fa-user' size='3x' />
+          </div>
+          <div className="logout-button">
+            <button onClick={this.handleLogout}>Logout</button>
+          </div>
         </div>
-
-        <button onClick={this.handleLogout}>Logout</button>
       </div>
     );
   }
 }
 
 export default Navigation;
+
+// <img src="http://res.cloudinary.com/rlee0525/image/upload/c_scale,w_75/v1484258090/userIcon_i3rk1y.png" />
