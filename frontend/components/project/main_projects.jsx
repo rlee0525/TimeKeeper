8ca8325e88212791ff5db3f3396ad4f9dd8d4973
Raymond Lee <raylee0525@gmail.com>
2017-01-15 02:13:06 -0800

    Implement buttons to link to projectdetail page

--- frontend/components/project/main_projects.jsx
+++ frontend/components/project/main_projects.jsx
@@ -5,10 +5,16 @@ import ProjectsProjectDetailContainer from './projects_project_detail_container'
 class MainProjects extends React.Component {
   constructor(props) {
     super(props);
+
+    this.handleProject = this.handleProject.bind(this);
   }
 
   componentDidMount() {
     this.props.requestProjects();
+  }
+
+  handleProject(id) {
+    return e => this.props.router.push(`/projects/${id}`);
   }
 
   render() {
@@ -26,11 +32,12 @@ class MainProjects extends React.Component {
           </div>
           <div className="projects-page-body">
             {Object.keys(projects).map(projectId => (
-              <li className="projects-li"
-                key={projectId}>
-                <ProjectsProjectDetailContainer
-                  project={ projects[projectId] } />
-              </li>
+              <button key={projectId} className="button-project-detail" onClick={this.handleProject(projectId)}>
+                <li className="projects-li">
+                  <ProjectsProjectDetailContainer
+                    project={ projects[projectId] } />
+                </li>
+              </button>
             ))}
           </div>
         </div>
