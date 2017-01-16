f8653eb2b43d7f5054d6640617a71b1ed738dfb9
Raymond Lee <raylee0525@gmail.com>
2017-01-15 18:58:17 -0800

    Changed routes to implement projects search attempt # 2 - refer to back

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create]
     resource :session, only: [:create, :destroy, :show]
-    resources :projects
+
+    resources :projects do
+      get "search", on: :collection
+    end
+
     resources :tasks, except: [:edit]
   end
 end
