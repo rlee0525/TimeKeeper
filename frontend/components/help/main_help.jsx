4008bcc8f1f28048849f9494f38e756192deabb5
Raymond Lee <raylee0525@gmail.com>
2017-01-20 13:52:57 -0800

    COMPLETE MVP!

--- frontend/components/help/main_help.jsx
+++ frontend/components/help/main_help.jsx
@@ -23,6 +23,39 @@ class MainHelp extends React.Component {
             </div>
           </div>
           <div className="help-page-body">
+            <div className='about'>
+              <div className='profile-pic'>
+                <img src="http://res.cloudinary.com/rlee0525/image/upload/v1484948855/Raymond_Lee_blorht.jpg" />
+              </div>
+              <div className='about-content'>
+                <a href='https://github.com/rlee0525/TimeKeeper'>
+                  <h1> TimeKeeper </h1>
+                </a>
+
+                <h3> TimeKeeper is a full-stack web application inspired by Toggl,
+                built on Ruby on Rails and React.js/Redux. The app features include:</h3>
+
+                <ul>
+                  <li>One-Click Time Tracking</li>
+                  <li>Sign Up / Sign In / Sign Out / Demo</li>
+                  <li>Production README</li>
+                  <li>Project CRUD</li>
+                  <li>Creating P</li>
+                  <li>Comments</li>
+                  <li> User pages</li>
+                </ul>
+
+                <h4> Reaons to use? </h4>
+                  <ul>
+                    <li>One-Click Time Tracking - It's insanely simple to use. And effective.</li>
+                    <li>Have an unlimited number of projects and tasks - No joke. Add as many as you need. It’s still free</li>
+                    <li>Add tasks - Gives you wiggle room for project tracking.</li>
+                    <li>View the summary report - Increase productivity through visualization</li>
+                  </ul>
+              </div>
+            </div>
+            <a href='https://github.com/rlee0525/TimeKeeper' target="_blank">Github</a>
+            <a href='https://www.linkedin.com/in/rlee0525' target="_blank">LinkedIn</a>
           </div>
         </div>
       </div>
