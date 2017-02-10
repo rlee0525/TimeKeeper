9d7968193a2ee4e7ae2253fe419949bdebd5c621
Raymond Lee <raylee0525@gmail.com>
2017-02-10 12:50:54 -0800

    Updated splash page position and logo

--- frontend/components/home/logo.jsx
+++ frontend/components/home/logo.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default () => (
-  <Link to="/">
+  <div>
     <img src="http://res.cloudinary.com/rlee0525/image/upload/c_scale,w_300/v1486607816/Logomakr_0ezcq5_zn4i8n.png" />
-  </Link>
+  </div>
 );
